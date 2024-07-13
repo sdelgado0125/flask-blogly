458bc8d20b680a9d0b88e9fe260c3cc2efe304c6
sdelgado0125 <sdelgado0125@gmail.com>
2024-07-13 13:40:02 -0400

    blog post part 2

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'ihaveasecret'
 
-
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
-
 
 connect_db(app)
 
 with app.app_context():
     db.create_all()
+    posts = Post.query.all()
+    print(posts)
 
 @app.route('/')
 def root():
@@ -23,7 +23,7 @@ def root():
 
 @app.route('/users')
 def users_index():
-    users = User.query.order_by(User.last_name, User.first_name).all()
+    users = User.query.all()
     print(users)
     return render_template('users/index.html', users=users)
 
@@ -48,7 +48,7 @@ def users_show(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('users/show.html', user=user)
 
-@app.route('/users/<int:user_id>/edit')
+@app.route('/users/<int:user_id>/edit', methods = ["GET"])
 def users_edit(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('users/edit.html', user=user)
@@ -72,3 +72,63 @@ def users_destroy(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+### post routes
+
+@app.route('/users/<int:user_id>/posts/new', methods=["GET"])
+def new_post_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/newpost.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def new_post(user_id):
+    new_post = Post(
+        title=request.form['title'],
+        content=request.form['content'],
+        user_id=user_id)
+    
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(url_for('users_show', user_id=user_id))
+
+@app.route('/posts')
+def posts_index():
+    posts = Post.query.all()
+    return render_template('posts/index.html', posts=posts)
+
+@app.route("/posts/<int:post_id>")
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/showpost.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["GET"])
+def posts_edit_form(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/editpost.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_edit(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    print(f"Edited Post ID: {post.id}, Title: {post.title}, Content: {post.content}")
+
+    return redirect(url_for('show_post', post_id=post.id))
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+
+    print(f"Deleted Post ID: {post.id}")
+
+    return redirect(url_for('users_show', user_id=post.user_id))
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -30,6 +31,34 @@ class User(db.Model):
                           nullable=False, 
                           default=DEFAULT_IMAGE_URL)
     
+    posts = db.relationship('Post', backref = 'user', cascade = 'all,delete-orphan')
+    
     def __repr__(self):
         return f'<User {self.id} {self.first_name} {self.last_name}>'
     
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    title = db.Column(db.Text,
+                    nullable=False,
+                    unique=True)
+    content = db.Column(db.Text,
+                    nullable=False)
+    created_at = db.Column(db.DateTime, 
+                    default=datetime.utcnow, 
+                    nullable=False)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey("users.id"),
+                        nullable = False)
+    
+    @property
+    def friendly_date(self):
+        return self.created_at.strftime('%Y-%m-%d %H:%M:%S')
